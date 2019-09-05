38e0a648ba1ede061daaca545224a3d843f3708a
grahamsawers <grahamsawers@gmail.com>
2019-09-05 16:01:19 +0100

    Add solution.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+  const readingList = document.querySelector('#reading-list');
+
+  const formSubmission = function(event) {
+    event.preventDefault();
+    console.log(event.target);
+    newReadingListItem = document.createElement('li');
+    newReadingListItem.classList.add('list-item');
+    readingList.appendChild(newReadingListItem);
+
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+
+    newItemTitle = document.createElement('p');
+    newItemTitle.classList.add('list-item-property-title');
+    newItemAuthor = document.createElement('p');
+    newItemAuthor.classList.add('list-item-property-author');
+    newItemCategory = document.createElement('p');
+
+    newReadingListItem.appendChild(newItemTitle);
+    newReadingListItem.appendChild(newItemAuthor);
+    newReadingListItem.appendChild(newItemCategory);
+
+    newItemTitle.textContent = title;
+    newItemAuthor.textContent = author;
+    newItemCategory.textContent = category;
+    newItemForm.reset();
+  };
+
+  const newItemForm = document.querySelector('#new-item-form');
+  newItemForm.addEventListener('submit', formSubmission);
+
+  const destoryEverything = function(event) {
+    readingList.innerHTML = '';
+  };
+
+  const deleteAll = document.querySelector('body > input[type=button]');
+  deleteAll.addEventListener('click', destoryEverything);
+});
